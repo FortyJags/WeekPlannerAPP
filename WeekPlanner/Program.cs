70e13d5fd7c8f932e8369fb953d9e7d586a5c57f
FortyJags <126119856+FortyJags@users.noreply.github.com>
2023-05-05 15:54:15 +0100

    Console.WriteLine added
    Added to test/learn about pull

--- WeekPlanner/Program.cs
+++ WeekPlanner/Program.cs
@@ -11,9 +11,11 @@ Load load = new Load();
 
 if (File.Exists("AgendaItems.txt")) { Console.WriteLine("File exists"); } else { File.Create("AgendaItems.txt"); }
 load.LoadData();
+Console.WriteLine("Testing pull);
 while (menu.IsInMenu == true)
 {
     menu.SwitchMenu();
+    
 }
 
 Save save = new Save(Monday.mondayAgenda, Tuesday.tuesdayAgenda, Wednesday.wednesdayAgenda, Thursday.thursdayAgenda, Friday.fridayAgenda, Saturday.saturdayAgenda, Sunday.sundayAgenda);
